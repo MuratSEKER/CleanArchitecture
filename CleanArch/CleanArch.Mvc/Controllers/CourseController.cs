1c68495651f8c2a67f7cd53158aace21eb9e2db1
Murat SEKER <benmuratseker@hotmail.com>
2020-10-25 21:02:22 +0300

    Presentation layer improved

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService courseService;
+        public CourseController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
