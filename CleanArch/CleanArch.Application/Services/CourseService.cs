e395df48268874174edb15caec98b91c7ef6c559
Murat SEKER <benmuratseker@hotmail.com>
2020-10-29 21:00:48 +0300

    Service updated

--- CleanArch/CleanArch.Application/Services/CourseService.cs
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -1,16 +1,28 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
 
 namespace CleanArch.Application.Services
 {
     public class CourseService : ICourseService
     {
-        private ICourseRepository courseRepository;
+        private readonly ICourseRepository courseRepository;
+        private readonly IMediatorHandler bus;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             this.courseRepository = courseRepository;
+            this.bus = bus;
+        }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var createCouserCommand = new CreateCourseCommand(
+                courseViewModel.Name, courseViewModel.Description, courseViewModel.ImageUrl);
+
+            bus.SendCommand<CreateCourseCommand>(createCouserCommand);
         }
 
         public CourseViewModel GetCourses()
